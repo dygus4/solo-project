088adf47002804f4ec3d7ca6767242383de9b853
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-05-07 23:11:26 +0200

    Add class active, JS

--- js/script.js
+++ js/script.js
@@ -96,3 +96,44 @@ document.getElementById('manager-button').addEventListener('click', function(e) 
     e.preventDefault();
     openModal('#manager');
 });
+
+//otwarcie poszczególnych stron )dodanie active do przycisku i artykułu
+
+
+const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
+    
+    console.log('Link was clicked!')
+    /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
+
+  /* add class 'active' to the clicked link */
+clickedElement.classList.add('active');
+console.log("clickedElement:", clickedElement)
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts div.active');
+
+  for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+  }
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
+  /* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
+  /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+}
+
+  
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
